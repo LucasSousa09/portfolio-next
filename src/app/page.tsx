47b989b145b993b506fbf99b5640a1bb6dc89e46
lucas <lucasrodrigues.sousa09@gmail.com>
2024-01-20 16:31:04 -0300

    Make adjusts to homepage styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,15 @@ import { CaretCircleDown } from "@phosphor-icons/react/dist/ssr";
 
 import { Paragraph } from "@/components/Paragraph";
 
-import logoImgDark from '../assets/logo-dark.png'
-
-import logoImgLight from '../assets/logo-light.png'
-
+import logoImgDark from '../assets/logo-dark.svg'
+import logoImgLight from '../assets/logo-light.svg'
 
 export default function Home() {
   return (
-    <div className="relative max-w-8xl w-full h-screen flex justify-center lg:justify-between items-center sm:items-start sm:pt-32 sm:px-8 md:px-12 sm:gap-12 md:gap-14 xl:px-32 ">
-      <Image className="absolute top-1/2 translate-y-[-50%] right-1/2 translate-x-1/2 sm:static sm:translate-y-0 sm:translate-x-0 mt-[-96px] sm:mt-4 xl:mt-12 h-40 w-40 sm:h-48 sm:w-48 lg:h-72 lg:w-72 opacity-10 sm:opacity-100" src={logoImgDark} alt="" />
+    <div className="relative max-w-8xl w-full h-[calc(100vh-52px)] sm:h-[calc(100vh-56px)] lg:h-[calc(100vh-92px)] flex justify-center lg:justify-between items-center sm:items-start sm:pt-32 sm:px-8 md:px-12 sm:gap-12 md:gap-14 xl:px-32 ">
+      
+      <Image className="hidden dark:block absolute top-1/2 translate-y-[-50%] right-1/2 translate-x-1/2 sm:static sm:translate-y-0 sm:translate-x-0 mt-[-96px] sm:mt-4 xl:mt-12 h-40 w-40 sm:h-48 sm:w-48 lg:h-72 lg:w-72 opacity-10 sm:opacity-100" src={logoImgDark} alt="" />
+      <Image className="dark:hidden block absolute top-1/2 translate-y-[-50%] right-1/2 translate-x-1/2 sm:static sm:translate-y-0 sm:translate-x-0 mt-[-96px] sm:mt-4 xl:mt-12 h-40 w-40 sm:h-48 sm:w-48 lg:h-72 lg:w-72 opacity-10 sm:opacity-100" src={logoImgLight} alt="" />
 
       <div className="mt-[-144px] sm:mt-0 flex flex-col items-center lg:items-start">
         <div className="max-w-[300px] sm:max-w-[400px] lg:max-w-[600px]">
@@ -26,8 +26,8 @@ export default function Home() {
           </Paragraph>
         </div>
 
-        <button className="group hover:animate-bounce mt-6 w-full sm:mt-12 flex items-center gap-2 text-zinc-400 text-shadow-light dark:text-shadow-dark font-semibold md:text-lg lg:text-2xl xl:text-3xl">
-          <CaretCircleDown className=" h-5 w-5 lg:h-10 lg:w-10" weight="fill" />
+        <button className="hover:animate-bounce mt-6 w-full sm:mt-12 flex items-center gap-2 text-zinc-400 text-shadow-light dark:text-shadow-dark font-semibold md:text-lg lg:text-2xl xl:text-3xl">
+          <CaretCircleDown className="h-5 w-5 lg:h-10 lg:w-10" weight="fill" />
           Conheça um pouco do meu trabalho!
         </button>
         
