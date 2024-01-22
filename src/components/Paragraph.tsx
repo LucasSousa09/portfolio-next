701e71e773b958c59e51d6f1739b09d4d4255077
lucas <lucasrodrigues.sousa09@gmail.com>
2024-01-22 13:15:53 -0300

    Adjust small css

--- src/components/Paragraph.tsx
+++ src/components/Paragraph.tsx
@@ -6,7 +6,7 @@ interface ParagraphProps {
 
 export function Paragraph({children}: ParagraphProps){
     return (
-        <p className="indent-8 text-sm sm:text-lg xl:text-xl leading-normal xl:leading-relaxed">
+        <p className="indent-4 sm:indent-6 lg:indent-8 text-sm sm:text-lg xl:text-xl leading-normal xl:leading-relaxed">
             {children}
         </p>
     )
