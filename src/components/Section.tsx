44a90e3e600269820407d109a6b86c385e3c77be
lucas <lucasrodrigues.sousa09@gmail.com>
2024-01-28 14:44:35 -0300

    Add animations to visible sections

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,13 +1,26 @@
-import { ReactNode } from "react"
+'use client'
+
+import { ReactNode, useEffect, useRef } from "react"
+import { motion } from 'framer-motion'
 
 type SectionProps = {
-    children: ReactNode
+    children: ReactNode,
+    id: string
 }
 
-export function Section({ children }: SectionProps){
+export function Section({ children, id }: SectionProps){
+    const ref = useRef<HTMLElement>(null)
+
     return (
-        <section className="flex flex-col items-center justify-center gap-8 lg:gap-16 w-full min-h-[calc(100vh-52px)] sm:min-h-[calc(100vh-56px)] lg:min-h-[calc(100vh-92px)]">
+        <motion.section
+            ref={ref}
+            id={id}
+            initial={{ opacity: 0, filter: 'blur(4px)' }}
+            whileInView={{ opacity: 1, filter: 'blur(0)' }}
+            transition={{ delay:.5, duration: 1.5 }}
+            className="flex flex-col items-center justify-center gap-8 lg:gap-16 w-full min-h-[calc(100vh-52px)] sm:min-h-[calc(100vh-56px)] lg:min-h-[calc(100vh-92px)] mb-[53px] sm:mb-[57px] lg:mb-[93px] last-of-type:mb-0"
+        >
             {children}
-        </section>
+        </motion.section>
     )
 }
