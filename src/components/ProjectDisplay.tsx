490c006c4bd77fcae1cd527b6227268cf256d17c
lucas <lucasrodrigues.sousa09@gmail.com>
2024-04-16 17:53:31 -0300

    Fix Responsiveness of ProjectDisplay component

--- src/components/ProjectDisplay.tsx
+++ src/components/ProjectDisplay.tsx
@@ -26,20 +26,20 @@ export function ProjectDisplay({
     links
 }: ProjectDisplayProps){
     return (
-        <div className="relative flex items-center px-16 justify-center h-40 sm:h-64 md:h-96 lg:h-[458px] rounded overflow-clip group">
+        <div className="relative flex items-center md:px-8 lg:px-16 justify-center h-40 sm:h-64 md:h-96 lg:h-[458px] rounded overflow-clip group">
             <Image className='absolute group-hover:brightness-50 top-0 left-0 bottom-0 right-0 object-cover w-full h-full transition-all ease-linear' src={image} alt="" width={600} height={400} />
-            <div className="hidden group-hover:flex flex-col items-start justify-center z-10">
+            <div className="hidden group-hover:flex flex-col items-center md:items-start justify-center z-10 w-full">
                 
-                <strong className='w-full text-center text-white lg:text-3xl mb-6'>{title}</strong>
+                <strong className='w-full text-center text-white text-xl lg:text-2xl xl:text-3xl mb-3 lg:mb-6'>{title}</strong>
                 
-                <p className="lg:text-xl lg:leading-normal text-white">
+                <p className="hidden sm:block lg:text-lg xl:text-xl lg:leading-normal text-white sm:max-w-96 sm:mb-4 md:mb-0">
                     {description}
                 </p>
-                <p className="lg:text-xl lg:leading-normal text-white">
+                <p className="hidden max-w-96 w-full md:block lg:text-lg xl:text-xl lg:leading-normal text-white">
                     {technologies.text}
                 </p>
                 
-                <ul className="lg:text-xl lg:leading-normal text-white list-disc lg:pl-7 lg:mb-11">
+                <ul className="hidden md:block lg:text-lg xl:text-xl lg:leading-normal text-white list-disc md:pl-5 md:mb-5">
                     {
                         technologies.techs.map((tech, idx) => (
                             <li key={idx}>{tech}</li>
@@ -47,12 +47,12 @@ export function ProjectDisplay({
                     }
                 </ul>
                 
-                <div className="flex w-full items-center justify-between">
-                    <Link href={links.githubLink} className="flex items-center gap-3 bg-white text-zinc-800 font-semibold lg:px-6 lg:py-3 hover:shadow-arr-dark hover:text-shadow-light-md transition-all ease-linear">
+                <div className="flex flex-col sm:flex-row sm:max-w-60 md:max-w-full gap-2 w-full items-center justify-between">
+                    <Link href={links.githubLink} className="flex items-center gap-3 bg-white text-zinc-800 text-sm sm:text-base font-semibold px-3 py-1 lg:px-6 lg:py-3 hover:shadow-arr-dark hover:text-shadow-light-md transition-all ease-linear">
                         <GithubLogo className="lg:h-8 lg:w-8" weight="bold"/>
                         Github
                     </Link>
-                    <Link href={links.websiteLink} className="flex items-center gap-3 bg-white text-zinc-800 font-semibold lg:px-6 lg:py-3 hover:shadow-arr-dark hover:text-shadow-light-md transition-all ease-linear">
+                    <Link href={links.websiteLink} className="flex items-center gap-3 bg-white text-zinc-800 text-sm sm:text-base font-semibold px-3 py-1 lg:px-6 lg:py-3 hover:shadow-arr-dark hover:text-shadow-light-md transition-all ease-linear">
                         <LinkIcon className="lg:h-8 lg:w-8" weight="bold"/>
                         Website
                     </Link>
