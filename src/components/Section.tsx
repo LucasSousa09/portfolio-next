36409277f5223c27fd415ea53c51ed7519368ccf
lucas <lucasrodrigues.sousa09@gmail.com>
2024-04-16 17:52:59 -0300

    Decrease animation duration

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -17,7 +17,7 @@ export function Section({ children, id }: SectionProps){
             id={id}
             initial={{ opacity: 0, filter: 'blur(4px)' }}
             whileInView={{ opacity: 1, filter: 'blur(0)' }}
-            transition={{ delay:.5, duration: 1.5 }}
+            transition={{ delay:.5, duration: .8 }}
             className="flex flex-col items-center justify-center gap-8 lg:gap-16 w-full min-h-[calc(100vh-52px)] sm:min-h-[calc(100vh-56px)] lg:min-h-[calc(100vh-92px)] mb-[53px] sm:mb-[57px] lg:mb-[93px] last-of-type:mb-0"
         >
             {children}
